af1fce1418ab158f30f1383db780702588485fc2
Moritz E. Pollack <moritze.pollack@gmail.com>
2023-03-14 22:01:24 +0100

    feat: implement **log.h** header file
    Log functions now print time and a string of the log level.
    
    See: Issue
    <https://github.com/unendlicherPing/ulog/issues/4>

--- src/log.c
+++ src/log.c
@@ -1,1 +1,28 @@
 #include <ulog/log.h>
+
+static char *log_string[] = {
+        "FATAL",
+        "ERROR",
+        "WARNING",
+        "INFO",
+        "DEBUG",
+};
+
+void ulog_log(int log_level, const char *fmt, ...) {
+    time_t raw_time;
+    struct tm *time_info;
+    char buffer[19];
+    va_list argp;
+
+    time(&raw_time);
+    time_info = localtime(&raw_time);
+
+    strftime(buffer, sizeof(buffer), "[%j:%m:%d] [%H:%M]", time_info);
+    free(time_info);
+
+    printf("%s [%s] ", buffer, log_string[log_level]);
+    va_start(argp, fmt);
+    vprintf(fmt, argp);
+    va_end(argp);
+    printf("\n");
+}
